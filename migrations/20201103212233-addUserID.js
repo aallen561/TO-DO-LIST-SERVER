37b31b42644aac36d3644dc5bf20eb911f8b3f73
Aubyn Allen <aubynallen@Aubyns-MBP.attlocal.net>
2020-11-03 17:42:26 -0500

    server is working thanks to sam

---
+++ migrations/20201103212233-addUserID.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('todos', "user_id", { type: Sequelize.INTEGER });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('todos', "user_id", {});
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
